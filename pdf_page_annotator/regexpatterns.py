c3946fab03f08cc52bf3d6b4aeb6dd22957fb0c6
Chinmay Shrivastava <79005030+ChinmayShrivastava@users.noreply.github.com>
2024-03-27 19:04:27 -0400

    Create regexpatterns.py

---
+++ pdf_page_annotator/regexpatterns.py
@@ -0,0 +1,21 @@
+import re
+
+# table of contents comes in many forms
+# the following are some patterns that can be used to identify the table of contents
+
+# 1: 1.2.3. Heading ..... 1 - 
+pattern1 = re.compile(r"table of contents", re.IGNORECASE)
+
+
+
+
+
+
+
+
+
+
+
+
+all_patterns = [
+]
